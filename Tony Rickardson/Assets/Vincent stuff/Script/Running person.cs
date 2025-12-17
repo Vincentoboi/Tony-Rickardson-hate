e4dfd677e1b2a005a618d258b41db7e7de2bf044
Vicnentoboi <vincent.waern@elev.ga.lbs.se>
2025-12-17 11:39:18 +0100

    mitt

---
+++ Tony Rickardson/Assets/Vincent stuff/Script/Running person.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Runningperson : MonoBehaviour
+{
+    GameObject player;
+    NavMeshAgent agent;
+    [SerializeField] LayerMask groundLayer, playerLayer;
+
+    //running around
+    Vector3 destPoint;
+    bool walkpointSet;
+    [SerializeField] float range;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        player = GameObject.Find("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        running();
+    }
+
+    void running()
+    {
+        if (!walkpointSet) SearchForDest();
+        if (walkpointSet) agent.SetDestination(destPoint);
+        if (Vector3.Distance(transform.position, destPoint) < 10) walkpointSet = false;
+    }
+
+    void SearchForDest()
+    {
+        float z = Random.Range(-range, range);
+        float x = Random.Range(-range, range);
+
+        destPoint = new Vector3(transform.position.x + x, transform.position.y, transform.position.z + z);
+
+        if(Physics.Raycast(destPoint, Vector3.down, groundLayer))
+        {
+            walkpointSet = true;
+        }
+    }
+}
